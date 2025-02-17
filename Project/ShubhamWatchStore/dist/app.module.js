e23df19515f3b761fa17a788ebb32a1041284bff
Kadam Shubham <kadamshu@ap.dtcs.local>
2025-02-17 16:41:14 +0530

    UI changes

--- Project/ShubhamWatchStore/dist/app.module.js
+++
@@ -1,7 +0,0 @@
-
-angular.module('ecomApp', ['ngRoute'])
-    .controller('AuthController', AuthController)
-    .service('AuthService', AuthService)
-    .controller('ProductController', ProductController)
-    .service('ProductService', ProductService)
-    .controller('OrderController', OrderController);
